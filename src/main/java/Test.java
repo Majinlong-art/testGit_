6a449bfb043b507a20e6ad7d4ebfc8dd285d10d6
Majinlong-art <63486888+Majinlong-art@users.noreply.github.com>
2020-04-11 21:52:42 +0800

    Update Test.java
    gitHub修改

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("master........"+"2321651635");
+        System.out.println("master........"+"2321651635"+"146545646");
     }
 }
